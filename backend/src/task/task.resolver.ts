1d716d83b3ff123f438a693334471fddcf53da88
Minoru Miyata <miyarappo@gmail.com>
2023-05-07 15:43:46 +0900

    task作成ができるようにする

--- backend/src/task/task.resolver.ts
+++ backend/src/task/task.resolver.ts
@@ -1,6 +1,7 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { TaskService } from './task.service';
 import { Task } from './models/task.model';
+import { CreateTaskInput } from './dto/createTask.input';
 
 @Resolver()
 export class TaskResolver {
@@ -9,5 +10,10 @@ export class TaskResolver {
   @Query(() => [Task], { nullable: 'items' })
   getTasks() {
     return this.taskService.getTasks();
+  }
+
+  @Mutation(() => Task)
+  createTask(@Args('createTaskInput') createTaskInput: CreateTaskInput): Task {
+    return this.taskService.createTask(createTaskInput);
   }
 }
